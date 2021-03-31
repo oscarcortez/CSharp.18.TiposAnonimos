156843ddc2137f1a11da5ebf5417807e7113da83
oscarcortez <oscarkortez@gmail.com>
2021-03-31 19:58:29 -0400

    Add project files.

---
+++ ConsoleApp4TiposAnonimos/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp4TiposAnonimos
+{
+    class Program
+    {       
+        /// <summary>
+        /// Tipos anonimos son clases sin nombre, no existe el codigo de dicha clase
+        /// 
+        /// </summary>
+        public class Persona
+        {
+            public string Nombre { get; set; }
+            public string Apellido { get; set; }
+            public int Edad { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            List<Persona> personas = new List<Persona>
+            {
+                new Persona() { Nombre = "Raul", Apellido = "Albiol", Edad = 33 },
+                new Persona() { Nombre = "Iago", Apellido = "Aspas", Edad = 30 }
+            };
+
+            var resultado = personas.Where(a => a.Edad == 30)
+                .Select(a => new { NombreCompleto = $"{a.Nombre} {a.Apellido}" }); // este caso devuelve un tipo de resultado distinto a la tabla inicial
+
+            foreach(var res in resultado)
+            {
+                Console.WriteLine(res.NombreCompleto);
+            }
+
+        }
+
+        
+    }
+}
